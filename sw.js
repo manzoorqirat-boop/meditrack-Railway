1d9dcbedb2d6c689938e2cb515c72af285595eb8
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-03-28 10:50:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v7';
+const CACHE = 'meditrack-v8';
 const FILES = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
